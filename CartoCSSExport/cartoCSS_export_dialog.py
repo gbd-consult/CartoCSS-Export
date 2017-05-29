8853a185b830cac14833a2375cdc6218b09d0006
towa <towarischtsch@users.noreply.github.com>
2017-05-29 10:07:53 +0200

    qgis plugin : initial commit

---
+++ CartoCSSExport/cartoCSS_export_dialog.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ CartoCSSExportDialog
+                                 A QGIS plugin
+ Exports .qgs Project files to CartoCSS
+                             -------------------
+        begin                : 2017-05-22
+        git sha              : $Format:%H$
+        copyright            : (C) 2017 by Geoinformatikbüro Dassau
+        email                : info@gbd-consult.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtGui, uic
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'cartoCSS_export_dialog_base.ui'))
+
+
+class CartoCSSExportDialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(CartoCSSExportDialog, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
